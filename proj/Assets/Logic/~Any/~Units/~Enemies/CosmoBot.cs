503afa944ca6358b5564cfe8c39b7ffa3a3b42ca
DiZLike <diz91@inbox.ru>
2020-12-20 17:18:00 +0300

    up

--- proj/Assets/Logic/~Any/~Units/~Enemies/CosmoBot.cs
+++ proj/Assets/Logic/~Any/~Units/~Enemies/CosmoBot.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class CosmoBot : Enemy
 {
     public LayerMask RayMask;
-    public float RayDidtanse = 100;
+    public float RayDidtanse = 200;
 
     BoxCollider sight;
     bool playerInSight = false;
@@ -14,34 +14,17 @@ public class CosmoBot : Enemy
     NavMeshAgent agent;
     Animator anim;
 
-    Transform bone1;
-    Transform bone2;
-
     new void Start()
     {
         base.Start();
         anim = GetComponent<Animator>();
 
         agent = GetComponent<NavMeshAgent>();
-        var comps = GetComponentsInChildren<BoxCollider>();
-        foreach (var item in comps)
-        {
-            if (item.name == "Sight")
-                sight = item;
-        }
-        var comps1 = GetComponentsInChildren<Transform>();
-        foreach (var item in comps1)
-        {
-            if (item.name == "Bone.004")
-                bone1 = item;
-            if (item.name == "Bone.009")
-                bone2 = item;
-        }
 
     }
 
-    // Update is called once per frame
-    new void Update()
+	// Update is called once per frame
+	new void Update()
     {
         base.Update();
         var tmp = CheckRayCast();
@@ -56,13 +39,10 @@ public class CosmoBot : Enemy
             float y = player.transform.position.y;
             float z = player.transform.position.z;
             Vector3 target = new Vector3(x, 0, z);
-            Vector3 target2 = new Vector3(0, y, 0);
             transform.LookAt(target);
-            //bone1.LookAt(target2);
-            //bone2.LookAt(target2);
 
             float dist = Vector3.Distance(transform.position, target);
-            if (dist > 5)
+            if (dist > 2)
             {
                 SetAnimMove();
                 agent.enabled = true;
